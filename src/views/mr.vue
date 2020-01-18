40d605d6310ab207a22d62d498eb4180f9a155d5
Roderick Qiu <scrisqiu@hotmail.com>
2020-01-18 18:55:41 +0800

    .gitignore modification && win works

--- src/views/mr.vue
+++ src/views/mr.vue
@@ -15,19 +15,23 @@
       <loading :active.sync="loading" :can-cancel="false" :is-full-page="true" loader="bars"></loading>
       <titlepart :canabout="true"></titlepart>
       <div id="notifies" style="-webkit-app-region: no-drag">
-        
-        I'll play for <br>
+        I'll play for
+        <br />
         <b-form-input
           id="playtime"
           v-model="playtime"
           type="number"
           required
-          @keyup.enter.native="start">
-        </b-form-input>
-        <b-popover target="playtime" :show.sync="timeNAN" triggers="" variant="danger" placement="bottom">
-          Please Enter an Integer
-        </b-popover>
-        Minutes <br>
+          @keyup.enter.native="start"
+        ></b-form-input>
+        <b-popover
+          target="playtime"
+          :show.sync="timeNAN"
+          triggers
+          variant="danger"
+          placement="bottom"
+        >Please Enter an Integer</b-popover>Minutes
+        <br />
         <b-btn variant="light" class="bfa" @click="start">Go</b-btn>
       </div>
       <b-modal
@@ -35,19 +39,53 @@
         ok-only
         :title="$t('stitle')"
         ref="smodal"
-        :ok-title="$t('close')">
+        :ok-title="$t('close')"
+      >
         <b-row>
-          <b-col sm="2" class="settingsleftbar"><small>{{ $t('slocale') }}</small></b-col>
-          <b-col><b-btn @click="i18nchinese">中文</b-btn><b-btn @click="i18nenglish">English</b-btn></b-col>
+          <b-col sm="2" class="settingsleftbar">
+            <small>{{ $t('slocale') }}</small>
+          </b-col>
+          <b-col>
+            <b-btn @click="i18nchinese">中文</b-btn>
+            <b-btn @click="i18nenglish">English</b-btn>
+          </b-col>
         </b-row>
         <b-row>
-          <b-col sm="2" class="settingsleftbar"><small>{{ $t('saboutbar') }}</small></b-col>
+          <b-col sm="2" class="settingsleftbar">
+            <small>{{ $t('saboutbar') }}</small>
+          </b-col>
           <b-col>
             <small class="form-text text-muted" id="abouttext">
-              {{ $t('sabout') }} {{ version }} <br> 
-              {{ $t('proud') }} <br> 
-              {{ $t('sauthor') }} <a class="text-muted-icon" href="https://github.com/scris" target="_blank"><i class="fab fa-github"></i></a>&nbsp;<a class="text-muted-icon" v-if="!isonios" href="mailto:slaaker@126.com" target="_blank"><i class="fa fa-envelope"></i></a> {{ $t('sothercontributor') }} <br>
-              {{ $t('scontribute') }} <a class="text-muted-icon" href="https://github.com/scris/mrnoplay/" target="_blank"><i class="fab fa-github"></i></a>
+              {{ $t('sabout') }} {{ version }}
+              <br />
+              {{ $t('proud') }}
+              <br />
+              {{ $t('sauthor') }}
+              <a
+                class="text-muted-icon"
+                href="https://github.com/scris"
+                target="_blank"
+              >
+                <i class="fab fa-github"></i>
+              </a>&nbsp;
+              <a
+                class="text-muted-icon"
+                v-if="!isonios"
+                href="mailto:slaaker@126.com"
+                target="_blank"
+              >
+                <i class="fa fa-envelope"></i>
+              </a>
+              {{ $t('sothercontributor') }}
+              <br />
+              {{ $t('scontribute') }}
+              <a
+                class="text-muted-icon"
+                href="https://github.com/scris/mrnoplay/"
+                target="_blank"
+              >
+                <i class="fab fa-github"></i>
+              </a>
             </small>
           </b-col>
         </b-row>
@@ -57,114 +95,115 @@
 </template>
 
 <script>
-  import loading from 'vue-loading-overlay';
-  import 'vue-loading-overlay/dist/vue-loading.css';
-  import { Plugins } from '@capacitor/core';
-  const { Storage } = Plugins;
-  import timepicker from 'vue-ctk-date-time-picker';
-  import '@/assets/vue-ctk-date-time-picker.css';
-  import titlepart from '@/components/titlepart'
-  var alarm = new Audio();
-  var _this = null;
-  var ipc = null;
-  if (process.env.VUE_APP_LINXF == "electron") {
-    ipc = window.require("electron").ipcRenderer; //use window.require instead of require
-  }
-  export default {
-    name: 'mr',
-    components: {
-      loading,
-      timepicker,
-      titlepart,
+import loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/vue-loading.css";
+import { Plugins } from "@capacitor/core";
+const { Storage } = Plugins;
+import timepicker from "vue-ctk-date-time-picker";
+import "@/assets/vue-ctk-date-time-picker.css";
+import titlepart from "@/components/titlepart";
+var alarm = new Audio();
+var _this = null;
+var ipc = null;
+if (process.env.VUE_APP_LINXF == "electron") {
+  ipc = window.require("electron").ipcRenderer; //use window.require instead of require
+}
+export default {
+  name: "mr",
+  components: {
+    loading,
+    timepicker,
+    titlepart
+  },
+  data() {
+    return {
+      time: "00:00",
+      loading: true,
+      iselectron: false,
+      isonios: false,
+      lang: "en",
+      iffetch: 0,
+      playtime: 5,
+      timeNAN: false,
+      version: ""
+    };
+  },
+  watch: {
+    async lang(val) {
+      this.storagesetlang(val);
+      this.$i18n.locale = val;
+    }
+  },
+  mounted: function() {
+    this.version = process.env.VUE_APP_VER;
+    this.i18nsetlang();
+    if (process.env.VUE_APP_LINXF == "electron") {
+      this.iselectron = true;
+    }
+    this.isonios = this.isiOS(navigator.userAgent);
+    _this = this;
+    this.loading = false;
+    this.storagesetjson("concentrated", true);
+    if (process.env.VUE_APP_LINXF == "electron") {
+      ipc.send("full-screen");
+    }
+    alarm.src = require("@/assets/scarymusic/" + this.rand(1, 17) + ".mp3");
+    setTimeout(this.timeout, 1000); //180000
+  },
+  beforeDestroy: function() {},
+  methods: {
+    isiPad(userAgent) {
+      return userAgent.indexOf("iPad") > -1;
     },
-    data() {
-      return {
-        time: '00:00',
-        loading: true,
-        iselectron: false,
-        isonios: false,
-        lang: 'en',
-        iffetch: 0,
-        playtime: 5,
-        timeNAN: false,
-        version: '',
-      };
+    isiPhone(userAgent) {
+      return userAgent.indexOf("iPhone") > -1;
     },
-    watch: {
-      async lang (val) {
-        this.storagesetlang(val);
-        this.$i18n.locale = val;
-      },
+    isiOS(userAgent) {
+      return this.isiPad(userAgent) || this.isiPhone(userAgent);
     },
-    mounted: function() {
-      this.version = process.env.VUE_APP_VER;
-      this.i18nsetlang();
-      if(process.env.VUE_APP_LINXF == 'electron') {
-        this.iselectron = true;
-      }
-      this.isonios = this.isiOS(navigator.userAgent);
-      _this = this;
-      this.loading = false;
-      this.storagesetjson('concentrated', true);
-      if (process.env.VUE_APP_LINXF == "electron") {
-        ipc.send('full-screen');
-      }
-      alarm.src = require("@/assets/scarymusic/"+this.rand(1,17)+".mp3");
-      setTimeout(this.timeout, 1000);//180000
+    async storagesetlang(val) {
+      await Storage.set({
+        key: "lang",
+        value: val
+      });
     },
-    beforeDestroy: function() {
-
+    async storagesetjson(key, val) {
+      await Storage.set({
+        key: key,
+        value: JSON.stringify(val)
+      });
     },
-    methods: {
-      isiPad (userAgent) {
-        return (userAgent.indexOf("iPad") > -1);
-      },
-      isiPhone (userAgent) {
-        return (userAgent.indexOf("iPhone") > -1);
-      },
-      isiOS (userAgent) {
-        return this.isiPad(userAgent) || this.isiPhone(userAgent);
-      },
-      async storagesetlang(val) {
-        await Storage.set({
-          key: 'lang',
-          value: val
+    async i18nsetlang() {
+      const keys = await Storage.keys();
+      if (keys.keys.indexOf("lang") != -1) {
+        const retlang = await Storage.get({ key: "lang" });
+        if (retlang.value != null) _this.lang = retlang.value;
+        else (_this.lang = "en"), _this.storagesetlang("en");
+      } else (_this.lang = "en"), _this.storagesetlang("en");
+      _this.$i18n.locale = _this.lang;
+    },
+    i18nchinese() {
+      this.lang = "cn";
+    },
+    i18nenglish() {
+      this.lang = "en";
+    },
+    start() {
+      if (/(^[1-9]\d*$)/.test(this.playtime)) {
+        this.storagesetjson("playtime", Number(this.playtime)).then(() => {
+          this.storagesetjson("concentrated", false);
+          if (process.env.VUE_APP_LINXF == "electron") {
+            ipc.send("normal-screen");
+          }
+          this.$router.push("timing");
         });
-      },
-      async storagesetjson(key,val) {
-        await Storage.set({
-          key: key,
-          value: JSON.stringify(val)
-        });
-      },
-      async i18nsetlang() {
-        const keys = await Storage.keys();
-        if(keys.keys.indexOf('lang') != -1) {
-          const retlang = await Storage.get({ key:'lang' });
-          if(retlang.value != null) _this.lang = retlang.value;
-          else _this.lang = 'en', _this.storagesetlang('en');
-        } else _this.lang = 'en', _this.storagesetlang('en');
-        _this.$i18n.locale = _this.lang;
-      },
-      i18nchinese() {
-        this.lang = 'cn';
-      },
-      i18nenglish() {
-        this.lang = 'en';
-      },
-      start() {
-        if((/(^[1-9]\d*$)/.test(this.playtime))) {
-          this.storagesetjson('playtime', Number(this.playtime)).then(() => {
-            this.storagesetjson('concentrated', false);
-            if (process.env.VUE_APP_LINXF == "electron") {
-              ipc.send('normal-screen');
-            }
-            this.$router.push('timing');
-          });
-        } else {
-          this.timeNAN = true;
-        }
+      } else {
+        this.timeNAN = true;
       }
+    },
+    rand(min, max) {
+      return min + Math.round((max - min) * Math.random());
     }
   }
+};
 </script>
--- src/views/over.vue
+++ src/views/over.vue
@@ -15,18 +15,17 @@
       <loading :active.sync="loading" :can-cancel="false" :is-full-page="true" loader="bars"></loading>
       <titlepart :canabout="false"></titlepart>
       <div id="notifies" style="-webkit-app-region: no-drag">
-        时间结束。<br>
-        3分钟内停止，否则将被惩罚。
+        时间结束。
+        <br />3分钟内停止，否则将被惩罚。
         （停止后，电脑将在1分钟内关闭，请先保存好自己的资料）
-        <br>
+        <br />
         <b-alert
           v-model="punishstart"
           class="position-fixed fixed-bottom m-0 rounded-0"
           style="z-index: 2000;"
           variant="danger"
-          dismissible>
-          惩罚现在开始。
-        </b-alert>
+          dismissible
+        >惩罚现在开始。</b-alert>
         <b-btn variant="light" class="bfa" @click="cancel">结束</b-btn>
       </div>
     </div>
@@ -34,134 +33,129 @@
 </template>
 
 <script>
-  import loading from 'vue-loading-overlay';
-  import 'vue-loading-overlay/dist/vue-loading.css';
-  import { Plugins } from '@capacitor/core';
-  const { Storage } = Plugins;
-  import timepicker from 'vue-ctk-date-time-picker';
-  import '@/assets/vue-ctk-date-time-picker.css';
-  import titlepart from '@/components/titlepart';
-  import notify from '@/components/linxf/notify';
-  var alarm = new Audio();
-  var _this = null;
-  var ipc = null;
-  if (process.env.VUE_APP_LINXF == "electron") {
-    ipc = window.require("electron").ipcRenderer; //use window.require instead of require
-  }
-  ipc.on('timingdone', function (event, arg) {
-   this.timing = false;
-  })
-  export default {
-    name: 'over',
-    components: {
-      loading,
-      timepicker,
-      titlepart,
+import loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/vue-loading.css";
+import { Plugins } from "@capacitor/core";
+const { Storage } = Plugins;
+import timepicker from "vue-ctk-date-time-picker";
+import "@/assets/vue-ctk-date-time-picker.css";
+import titlepart from "@/components/titlepart";
+import notify from "@/components/linxf/notify";
+var alarm = new Audio();
+var _this = null;
+var ipc = null;
+if (process.env.VUE_APP_LINXF == "electron") {
+  ipc = window.require("electron").ipcRenderer; //use window.require instead of require
+  ipc.on("timingdone", function(event, arg) {
+    this.timing = false;
+  });
+}
+export default {
+  name: "over",
+  components: {
+    loading,
+    timepicker,
+    titlepart
+  },
+  data() {
+    return {
+      time: "00:00",
+      loading: true,
+      iselectron: false,
+      isonios: false,
+      lang: "cn",
+      timing: false,
+      playtime: 0,
+      displaytime: "0:00",
+      lefttime: 0,
+      punishstart: false,
+      notifymessage: ["开始工作啦", "工作时间开始"]
+    };
+  },
+  watch: {
+    async lang(val) {
+      this.storagesetlang(val);
+      this.$i18n.locale = val;
+    }
+  },
+  mounted: function() {
+    this.version = process.env.VUE_APP_VER;
+    this.i18nsetlang();
+    if (process.env.VUE_APP_LINXF == "electron") {
+      this.iselectron = true;
+    }
+    this.isonios = this.isiOS(navigator.userAgent);
+    _this = this;
+    this.loading = false;
+    this.timing = true;
+    alarm.src = require("@/assets/scarymusic/" + this.rand(1, 17) + ".mp3");
+    setTimeout(this.timeout, 1000); //180000
+  },
+  beforeDestroy: function() {},
+  methods: {
+    isiPad(userAgent) {
+      return userAgent.indexOf("iPad") > -1;
     },
-    data() {
-      return {
-        time: '00:00',
-        loading: true,
-        iselectron: false,
-        isonios: false,
-        lang: 'cn',
-        timing: false,
-        playtime: 0,
-        displaytime: "0:00",
-        lefttime: 0,
-        punishstart: false,
-        notifymessage: [
-          '开始工作啦',
-          '工作时间开始',
-        ],
-      };
+    isiPhone(userAgent) {
+      return userAgent.indexOf("iPhone") > -1;
     },
-    watch: {
-      async lang (val) {
-        this.storagesetlang(val);
-        this.$i18n.locale = val;
-      },
+    isiOS(userAgent) {
+      return this.isiPad(userAgent) || this.isiPhone(userAgent);
     },
-    mounted: function() {
-      this.version = process.env.VUE_APP_VER;
-      this.i18nsetlang();
-      if(process.env.VUE_APP_LINXF == 'electron') {
-        this.iselectron = true;
+    async storagesetlang(val) {
+      await Storage.set({
+        key: "lang",
+        value: val
+      });
+    },
+    async storagesetjson(key, val) {
+      await Storage.set({
+        key: key,
+        value: JSON.stringify(val)
+      });
+    },
+    async i18nsetlang() {
+      const keys = await Storage.keys();
+      if (keys.keys.indexOf("lang") != -1) {
+        const retlang = await Storage.get({ key: "lang" });
+        if (retlang.value != null) _this.lang = retlang.value;
+        else (_this.lang = "en"), _this.storagesetlang("en");
+      } else (_this.lang = "en"), _this.storagesetlang("en");
+      _this.$i18n.locale = _this.lang;
+    },
+    i18nchinese() {
+      this.lang = "cn";
+    },
+    i18nenglish() {
+      this.lang = "en";
+    },
+    cancel() {
+      this.storagesetjson("concentrated", true);
+      if (process.env.VUE_APP_LINXF == "electron") {
+        ipc.send("full-screen");
+        ipc.send("shutdown");
+      } else {
+        this.timing = false;
       }
-      this.isonios = this.isiOS(navigator.userAgent);
-      _this = this;
-      this.loading = false;
-      this.timing = true;
-      alarm.src = require("@/assets/scarymusic/"+this.rand(1,17)+".mp3");
-      setTimeout(this.timeout, 1000);//180000
+      this.$router.push("/");
     },
-    beforeDestroy: function() {
-
+    interval() {
+      if (this.timing == true) {
+        this.punishstart = true;
+        notify.methods.send({
+          title: "现在停止",
+          id: 1,
+          message: this.notifymessage[this.rand(0, this.notifymessage.length)]
+        });
+        alarm.play();
+      }
     },
-    methods: {
-      isiPad (userAgent) {
-        return (userAgent.indexOf("iPad") > -1);
-      },
-      isiPhone (userAgent) {
-        return (userAgent.indexOf("iPhone") > -1);
-      },
-      isiOS (userAgent) {
-        return this.isiPad(userAgent) || this.isiPhone(userAgent);
-      },
-      async storagesetlang(val) {
-        await Storage.set({
-          key: 'lang',
-          value: val
-        });
-      },
-      async storagesetjson(key,val) {
-        await Storage.set({
-          key: key,
-          value: JSON.stringify(val)
-        });
-      },
-      async i18nsetlang() {
-        const keys = await Storage.keys();
-        if(keys.keys.indexOf('lang') != -1) {
-          const retlang = await Storage.get({ key:'lang' });
-          if(retlang.value != null) _this.lang = retlang.value;
-          else _this.lang = 'en', _this.storagesetlang('en');
-        } else _this.lang = 'en', _this.storagesetlang('en');
-        _this.$i18n.locale = _this.lang;
-      },
-      i18nchinese() {
-        this.lang = 'cn';
-      },
-      i18nenglish() {
-        this.lang = 'en';
-      },
-      cancel() {
-        this.storagesetjson('concentrated', true);
-        if (process.env.VUE_APP_LINXF == "electron") {
-          ipc.send('full-screen');
-          ipc.send('shutdown');
-        } else {
-          this.timing = false;
-        }
-        this.$router.push('/');
-      },
-      interval() {
-        if(this.timing == true) {
-          this.punishstart = true;
-          notify.methods.send({
-            title: "现在停止",
-            id: 1,
-            message: this.notifymessage[this.rand(0,this.notifymessage.length)]
-          });
-          alarm.play();
-        }
-      },
-      timeout() {
-        setInterval(this.interval, 2000);
-      },
-      rand(min,max) {
-        return (min + Math.round((max - min) * Math.random()));
-      },
+    timeout() {
+      setInterval(this.interval, 2000);
+    },
+    rand(min, max) {
+      return min + Math.round((max - min) * Math.random());
     }
   }
+};
 </script>
