9c2dc949b97a86282ab0999684277800bff3f28b
Tianze Ds Qiu <slaaker@126.com>
2020-02-23 12:26:52 +0800

    fix a production bug on css
    set css.extract to false in vue.config.js
    thanks to https://blog.csdn.net/m0_37792354/article/details/82012278

--- vue.config.js
+++ vue.config.js
@@ -1,54 +1,66 @@
 const path = require('path')
 const UglifyPlugin = require('uglifyjs-webpack-plugin')
+var MiniCssExtractPlugin = require('mini-css-extract-plugin')
 process.env.VUE_APP_VER = require('./package.json').version
 module.exports = {
-	publicPath: './',
-    outputDir: 'www',
-    productionSourceMap: false,
-    configureWebpack: (config) => {
-        if (process.env.NODE_ENV == 'production') {
-            // 为生产环境修改配置
-            config.mode = 'production'
-            // 将每个依赖包打包成单独的js文件
-            let optimization = {
-              minimizer: [new UglifyPlugin({
-                  uglifyOptions: {
-                      warnings: false,
-                      compress: {
-                        drop_console: true, 
-                        drop_debugger: false,
-                        pure_funcs: ['console.log'] 
-                      }
-                  }
-               })]
-            }
-            let node = {
-                __filename: true,
-                __dirname: true,
-                console: true,
-            };  
-            Object.assign(config, {
-              optimization, node
-            })
-        } else {
-            // 为开发环境修改配置
-            config.mode = 'development'
-            let node = {
-                __filename: true,
-                __dirname: true
-            };  
-            Object.assign(config, {
-              node
-            })
-        }   
-    },
-    chainWebpack: config => {
-        config.module
-          .rule("i18n")
-          .resourceQuery(/blockType=i18n/)
-          .type('javascript/auto')
-          .use("i18n")
-            .loader("@kazupon/vue-i18n-loader")
-            .end();
+  publicPath: './',
+  outputDir: 'www',
+  productionSourceMap: false,
+  css: {
+    extract: false,
+    loaderOptions: {
+      sass: {
+        sassOptions: {
+          minimize: false
+        }
+      },
     }
+  },
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV == 'production') {
+      // 为生产环境修改配置
+      config.mode = 'production'
+      // 将每个依赖包打包成单独的js文件
+      let optimization = {
+        minimizer: [new UglifyPlugin({
+          uglifyOptions: {
+            warnings: false,
+            compress: {
+              drop_console: true,
+              drop_debugger: false,
+              pure_funcs: ['console.log']
+            }
+          }
+        })],
+      };
+      let node = {
+        __filename: true,
+        __dirname: true,
+        console: true,
+      };
+      Object.assign(config, {
+        optimization,
+        node,
+      })
+    } else {
+      // 为开发环境修改配置
+      config.mode = 'development'
+      let node = {
+        __filename: true,
+        __dirname: true
+      };
+      Object.assign(config, {
+        node,
+      })
+    }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule("i18n")
+      .resourceQuery(/blockType=i18n/)
+      .type('javascript/auto')
+      .use("i18n")
+      .loader("@kazupon/vue-i18n-loader")
+      .end();
+  },
 }
