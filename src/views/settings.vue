700c6b478d9e074a3b99b875ffa8bdba211f2a70
Tianze Ds Qiu <slaaker@126.com>
2020-02-24 16:36:07 +0800

    small ui modifications
    removed some unused code snippets

--- src/views/settings.vue
+++ src/views/settings.vue
@@ -2,16 +2,6 @@
 <template>
   <div>
     <notify ref="notify"></notify>
-    <!--
-    <div id="planselectorcontainer" class="linediv">
-      <div class="toptext btn"><b-btn variant="light" class="new on topicon">Mr Noplay</b-btn></div>
-      <div class="btn closewindowcontainer" style="-webkit-app-region: no-drag; -webkit-user-select: none">
-        <b-btn variant="light" class="new on topicon" v-b-modal.settingspanel><i class="fa fa-sliders-h"></i></b-btn>
-        <a v-if="iselectron" class="btn new on closewindow topicon btn-light" href="javascript:window.close()"><i class="fa fa-times"></i></a>
-      </div>
-    </div>
-    <div id="undergradient" class="linediv"></div>
-    -->
     <div class="container setting-container">
       <div class="setting-left" style="-webkit-app-region: no-drag">
         <div id="settingsnbsppart"></div>
@@ -45,7 +35,7 @@
             <b class="label">{{ $t("update") }}</b>
             <br />
             <div>
-              <a class="tutorial-a" @click="check" v-if="!checking">{{ $t("checkforupdate") }}</a>
+              <a class="tutorial-a check-a" @click="check" v-if="!checking">{{ $t("checkforupdate") }}</a>
               <span class="label" @click="check" v-if="checking">{{ $t("checking") }}</span>
             </div>
           </div>
--- src/views/tutorial.vue
+++ src/views/tutorial.vue
@@ -1,16 +1,6 @@
 <i18n src="@/assets/json/lang.json"></i18n>
 <template>
   <div>
-    <!--
-    <div id="planselectorcontainer" class="linediv">
-      <div class="toptext btn"><b-btn variant="light" class="new on topicon">Mr Noplay</b-btn></div>
-      <div class="btn closewindowcontainer" style="-webkit-app-region: no-drag; -webkit-user-select: none">
-        <b-btn variant="light" class="new on topicon" v-b-modal.settingspanel><i class="fa fa-sliders-h"></i></b-btn>
-        <a v-if="iselectron" class="btn new on closewindow topicon btn-light" href="javascript:window.close()"><i class="fa fa-times"></i></a>
-      </div>
-    </div>
-    <div id="undergradient" class="linediv"></div>
-    -->
     <div class="container">
       <div id="tutorialnbsppart"></div>
       <div id="main" style="-webkit-app-region: no-drag">
